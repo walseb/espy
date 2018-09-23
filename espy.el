b91c7e5278e691f3659e731c48aaa1cb3621b4f1
Sebastian Wålinder <s.walinder@gmail.com>
2018-09-23 23:52:32 +0200

    Updated espy.el commentary

--- espy.el
+++ espy.el
@@ -22,12 +22,12 @@
 ;;; Commentary:
 
 ;; To espy is to see something at a distance which is what this package is about.
-;; This package allows users to fetch a password from a file without visiting
-;; it. It gathers all headers containing passwords defined by
-;; `espy-header-prefix' and `espy-pass-prefix' and presents them to the user.
-;; The choosen password gets added to the kill ring.
+;; This package allows users to fetch a  user and password from a file
+;; without visiting it. It gathers all headers containing passwords or
+;; usernames as defined by `espy-header-prefix' and `espy-pass-prefix' and
+;; presents them to the user. The choosen password gets added to the kill ring.
 ;;
-;; For example using a password file with the content of
+;; For example using a file with the content of
 ;; (with no whitespace before any prefix)
 ;;
 ;;    *** Header1
@@ -36,10 +36,10 @@
 ;;
 ;; and running
 ;;
-;;    (espy)
+;;    (espy-get-pass)
 ;;
 ;; would result in one entry `Header1' being selectable. Choosing this entry
-;; results in `Password1' being copied to the kill-ring
+;; would result in `Password1' being copied to the kill-ring
 
 ;;; Code:
 

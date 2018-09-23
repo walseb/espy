fb79de64d48b4a17ff84b504f9371bffb985390a
Sebastian Wålinder <s.walinder@gmail.com>
2018-09-23 23:21:02 +0200

    Added username support, command line clipboard (e.g. xclipboard) support

--- espy.el
+++ espy.el
@@ -24,14 +24,15 @@
 ;; To espy is to see something at a distance which is what this package is about.
 ;; This package allows users to fetch a password from a file without visiting
 ;; it. It gathers all headers containing passwords defined by
-;; `espy-header-prefix' and `espy-password-prefix' and presents them to the user.
+;; `espy-header-prefix' and `espy-pass-prefix' and presents them to the user.
 ;; The choosen password gets added to the kill ring.
 ;;
 ;; For example using a password file with the content of
 ;; (with no whitespace before any prefix)
 ;;
 ;;    *** Header1
-;;    >> Password1
+;;    user: User1
+;;    pass: Password1
 ;;
 ;; and running
 ;;
@@ -42,6 +43,12 @@
 
 ;;; Code:
 
+(defvar espy-clipboard-command nil
+  "If non-nil, copy passwords and usernames using the string inserted as base.
+
+Espy runs the command as a standard command line command in this order:
+`espy-clipboard-command' + password-or-user-string")
+
 (defvar espy-password-file "~/password.org.gpg"
   "The file to pull passwords from.")
 
@@ -51,11 +58,16 @@
 Expected to be repeated 1 or more times before a
 whitespace followed by a password in password file.")
 
-(defvar espy-password-prefix ">>"
+(defvar espy-user-prefix "user:"
   "A string prefixing passwords.")
 
-(defun espy-get-password-headers ()
-  "Fetches all headers containing a password in a file."
+(defvar espy-pass-prefix "pass:"
+  "A string prefixing passwords.")
+
+(defun espy-get-headers-with-content (content-prefix)
+  "Fetches all headers containing a password in a file.
+
+CONTENT-PREFIX is the prefix expect before content."
   (with-temp-buffer
     (insert-file-contents espy-password-file)
     (goto-char (point-min))
@@ -65,7 +77,7 @@ whitespace followed by a password in password file.")
 	  (ignore-errors
 	    ;; Go to next header in case last header had more than 1 password
 	    (re-search-forward (concat "^" espy-header-prefix "+\s"))
-	    (re-search-forward (concat "^" espy-password-prefix))
+	    (re-search-forward (concat "^" content-prefix))
 	    (re-search-backward (concat "^" espy-header-prefix "+\s"))
 	    ;; Search-backward puts the cursor at column position 0, fix this
 	    (re-search-forward "\s"))
@@ -73,22 +85,39 @@ whitespace followed by a password in password file.")
 	(push (buffer-substring-no-properties (point) (line-end-position)) found-headers))
       found-headers)))
 
-;;;###autoload
-(defun espy ()
-  "Scans `espy-password-file' and prompts user for password entry.
+(defun espy-get-content (prompt-string content-prefix)
+  "Scans `espy-password-file' and prompts user for all headers with content.
 
-After user has selected a password entry it is copied to the kill ring."
-  (interactive)
+Content is defined by text beginning with CONTENT-PREFIX.
+`PROMPT-STRING' is displayed among prompt.
+After the user has selected a entry it is copied to the kill ring
+or a command is run on it as defined by `espy-clipboard-command'."
   (with-temp-buffer
     (insert-file-contents espy-password-file)
     (goto-char (point-min))
     ;; Go to selected heading
     (re-search-forward
      (concat espy-header-prefix " "
-	     (completing-read "Get password: " (espy-get-password-headers))))
+	     (completing-read prompt-string (espy-get-headers-with-content content-prefix))))
     ;; Go to heading password
-    (re-search-forward (concat "^" espy-password-prefix "\s"))
-    (kill-new (buffer-substring-no-properties (point) (line-end-position)))))
+    (re-search-forward (concat "^" content-prefix "\s"))
+    (buffer-substring-no-properties (point) (line-end-position))))
+
+;;;###autoload
+(defun espy-get-user ()
+  "Prompts user for user name to copy to killring."
+  (interactive)
+  (if espy-clipboard-command
+      (shell-command (concat espy-clipboard-command (espy-get-content "Get user: " espy-user-prefix)))
+    (kill-new (espy-get-content "Get user: " espy-user-prefix))))
+
+;;;###autoload
+(defun espy-get-pass ()
+  "Prompts user for password to copy to killring."
+  (interactive)
+  (if espy-clipboard-command
+      (shell-command (concat espy-clipboard-command (espy-get-content "Get password: " espy-pass-prefix)))
+    (kill-new (espy-get-content "Get password: " espy-pass-prefix))))
 
 (provide 'espy)
 
